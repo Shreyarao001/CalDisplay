4571211283b999af1480bbf7205ef894f21f4dcb
shreya <shreyachinmayi@gmail.com>
2024-02-16 14:33:01 +0530

    SECOND

---
+++ Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalDisplay
+{
+    public class Calculator
+    {
+        public void Add(double x, double y)
+        {
+            Console.WriteLine("Result after addition:\t" + (x + y));
+        }
+        public void Sub(double x, double y)
+        {
+            Console.WriteLine("Result after Substraction:\t" + (x - y));
+        }
+        public void Multi(double x, double y)
+        {
+            Console.WriteLine("Result after Multiplication:\t" + (x * y));
+        }
+        public void Div(double x, double y)
+        {
+            Console.WriteLine("Result after Division:\t" + (x / y));
+        }
+        public void Avg(double x, double y)
+        {
+            Console.WriteLine("Average of  =\t: " + (x + y) / 2);
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CalDisplay;
+Calculator calc = new Calculator();
+Console.WriteLine("Add Call");
+calc.Add(12, 88);
+Console.WriteLine("Multiply Call");
+calc.Multi(12.2, 16.5);
+Console.WriteLine("Substraction call");
+calc.Sub(985641, 785412);
+Console.WriteLine("Average Call");
+calc.Avg(8, 98);
